3a07a76a58167a9a8e5661303ec09a93ab214c21
samrudhhi2503 <samrudhhipawar6659@gmail.com>
2024-08-05 10:43:55 +0530

    Initial commit

---
+++ app/src/main/java/com/samruddhi/demo/MyApplication.java
@@ -0,0 +1,20 @@
+package com.samruddhi.demo;
+
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+
+import arrowwould.in.admobopenads.AppOpenManager;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(this);
+
+        new AppOpenManager(this, "ca-app-pub-3940256099942544/9257395921");
+
+    }
+
+}
